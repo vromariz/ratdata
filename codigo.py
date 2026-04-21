0a450342cdf837db3ff0f6eda37aae8c97db070a
vromariz <viniciusrfscabral@gmail.com>
2026-04-21 17:12:35 -0300

    filtro fe item

--- codigo.py
+++ codigo.py
@@ -1,16 +1,23 @@
 import pandas as pd
 
-ITENS_PATH = "/workspaces/ratdata/DADOS/ITENS_PROVA_2024.csv"
+pd.set_option('display.max_columns', None)   # mostra todas as colunas
+pd.set_option('display.width', None)         # evita quebra de linha
+pd.set_option('display.max_colwidth', None)  # mostra conteúdo completo
+
+ITENS_PATH = "/home/vinicius/Downloads/microdados_enem_2024/DADOS/ITENS_PROVA_2024.csv"
 
 itens = pd.read_csv(ITENS_PATH, sep=';', encoding='latin-1')
 
 resultado = itens[
-    (itens['CO_POSICAO'].isin([139, 140, 149, 165])) &
-    (itens['SG_AREA'] == 'MT') &
-    (itens['TX_COR'] == 'AMARELA')
+    (itens['CO_ITEM'].isin([60335, 111706, 125923, 29021]))&
+    (itens['TX_COR'].isin(['AZUL', 'AMARELA', 'LARANJA', 'LEITOR TELA', 'ROXA', 'VERDE', 'BRANCA',
+ 'CINZA']))&
+    (itens['IN_ITEM_ABAN'] == 0)
 ]
 
-print(resultado)
+print(resultado.value_counts)
+
+print(itens['TX_COR'].unique())
 
 resultado = itens[
     (itens['SG_AREA'] == 'MT') &
