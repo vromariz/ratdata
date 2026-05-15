ac41a3b00639504da4555f3cd3482a60178f4dd5
vromariz <viniciusrfscabral@gmail.com>
2026-05-15 15:34:14 -0300

    para melhorar

--- teste.py
+++ teste.py
@@ -1,125 +1,71 @@
 import pandas as pd
 
-ITENS_PATH = "/home/vinicius/Downloads/microdados_enem_2024/DADOS/ITENS_PROVA_2024.csv"
+arquivo = "/home/vinicius/Downloads/microdados_enem_2024/DADOS/RESULTADOS_2024.csv"
 
-MICRODATA_PATH = "/home/vinicius/Downloads/microdados_enem_2024/DADOS/RESULTADO_2024.csv"
+colunas = [
+    "SG_UF_PROVA",
+    "CO_PROVA_MT"
+]
 
-itens = pd.read_csv(
-    ITENS_PATH,
-    sep=';',
-    encoding='latin-1'
-)
+print("Lendo dados...")
 
 df = pd.read_csv(
-    MICRODATA_PATH,
-    sep=';',
-    encoding='latin-1'
+    arquivo,
+    sep=";",
+    encoding="latin1",
+    usecols=colunas,
+    low_memory=False
 )
-
-itens_alvo = [60335, 111706, 125923, 29021]
-
-itens = itens[
-    (itens['SG_AREA'] == 'MT') &
-    (itens['IN_ITEM_ABAN'] == 0)
-]
-
-mapa_posicoes = {}
-
-for _, row in itens.iterrows():
-
-    chave = (row['CO_PROVA'], row['CO_ITEM'])
-
-    mapa_posicoes[chave] = row['CO_POSICAO']
 
 df = df[
-    df['TX_RESPOSTAS_MT'].notna()
-]
+    df["SG_UF_PROVA"] == "PE"
+].copy()
 
-def acertou_todas(row):
+print("Quantidade total PE:", len(df))
 
-    prova = row['CO_PROVA_MT']
+mapa_provas = {
+    "AZUL": 1407,
+    "AMARELA": 1408,
+    #"VERDE": 1409,
+    #"CINZA": 1410,
+    #"VERDE2": 1411,
+    #"VERDE3": 1412,
+    #"LARANJA": 1413,
+    #"LARANJA2": 1414,
+    #"ROXA": 1415,
+    #"ROXA2": 1416,
+    #"ROXA3": 1417,
+    #"LEITOR TELA": 1418
+}
 
-    respostas = row['TX_RESPOSTAS_MT']
+resultado = []
 
-    gabarito = row['TX_GABARITO_MT']
+for nome_cor, codigo_prova in mapa_provas.items():
 
-    for item in itens_alvo:
+    quantidade = (
+        df["CO_PROVA_MT"] == codigo_prova
+    ).sum()
 
-        chave = (prova, item)
+    print(f"{nome_cor}: {quantidade}")
 
-        if chave not in mapa_posicoes:
-            return 0
-
-        co_posicao = mapa_posicoes[chave]
-
-        indice = co_posicao - 136
-
-        resposta_aluno = respostas[indice]
-
-        resposta_correta = gabarito[indice]
-
-        if resposta_aluno != resposta_correta:
-            return 0
-
-    return 1
-
-df['ACERTOU_TODAS'] = df.apply(
-    acertou_todas,
-    axis=1
-)
-
-df_filtrado = df[
-    df['ACERTOU_TODAS'] == 1
-]
-
-def calcular_percentual(row):
-
-    respostas = row['TX_RESPOSTAS_MT']
-
-    gabarito = row['TX_GABARITO_MT']
-
-    acertos = 0
-
-    for i in range(45):
-
-        if respostas[i] == gabarito[i]:
-            acertos += 1
-
-    return (acertos / 45) * 100
-
-df_filtrado['PERCENTUAL_MT'] = df_filtrado.apply(
-    calcular_percentual,
-    axis=1
-)
-
-resultado = (
-    df_filtrado
-    .groupby('NO_MUNICIPIO_PROVA')
-    .agg({
-        'NU_INSCRICAO': 'count',
-        'PERCENTUAL_MT': 'mean',
-        'NU_NOTA_MT': 'mean'
+    resultado.append({
+        "COR": nome_cor,
+        "CO_PROVA_MT": codigo_prova,
+        "QUANTIDADE": quantidade
     })
-    .rename(columns={
-        'NU_INSCRICAO': 'QTD_ALUNOS',
-        'PERCENTUAL_MT': 'MEDIA_PERCENTUAL_MT',
-        'NU_NOTA_MT': 'MEDIA_NOTA_MT'
-    })
-    .sort_values(
-        'QTD_ALUNOS',
-        ascending=False
-    )
-)
+
+df_final = pd.DataFrame(resultado)
 
 print("\nRESULTADO FINAL\n")
+print(df_final)
 
-print(resultado)
+saida = "/home/vinicius/Downloads/quantidade_provas_pe.csv"
 
-resultado.to_csv(
-    "resultado_municipios.csv"
-)
-
-df_filtrado.to_csv(
-    "alunos_acertaram_todas.csv",
+df_final.to_csv(
+    saida,
+    sep=";",
     index=False
 )
+
+print("\nArquivo salvo em:")
+print(saida)
